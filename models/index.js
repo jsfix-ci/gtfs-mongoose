7224141770471d68329245514c4bf948ee913ebe
Kyle Landell <kyle.landell@gmail.com>
2017-02-19 20:01:05 -0500

    fix .codeclimate.yml, refactor models/index.js

--- models/index.js
+++ models/index.js
@@ -1,30 +1,16 @@
-const Agency = require('./agency');
-const CalendarDate = require('./calendar-date');
-const Calendar = require('./calendar');
-const FareAttribute = require('./fare-attribute');
-const FareRule = require('./fare-rule');
-const FeedInfo = require('./feed-info');
-const Frequency = require('./frequency');
-const Route = require('./route');
-const Shape = require('./shape');
-const StopTime = require('./stop-time');
-const Stop = require('./stop');
-const Transfer = require('./transfer');
-const Trip = require('./trip');
-
-
+/* eslint global-require: 0 */
 module.exports = {
-    Agency,
-    CalendarDate,
-    Calendar,
-    FareAttribute,
-    FareRule,
-    FeedInfo,
-    Frequency,
-    Route,
-    Shape,
-    StopTime,
-    Stop,
-    Transfer,
-    Trip,
+    Agency: require('./agency'),
+    CalendarDate: require('./calendar-date'),
+    Calendar: require('./calendar'),
+    FareAttribute: require('./fare-attribute'),
+    FareRule: require('./fare-rule'),
+    FeedInfo: require('./feed-info'),
+    Frequency: require('./frequency'),
+    Route: require('./route'),
+    Shape: require('./shape'),
+    StopTime: require('./stop-time'),
+    Stop: require('./stop'),
+    Transfer: require('./transfer'),
+    Trip: require('./trip'),
 };
